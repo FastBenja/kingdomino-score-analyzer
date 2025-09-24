07f9696cefd1859b80f6306383e4508e6a2f5284
Petur Hammer <hammerpetur@gmail.com>
2025-09-24 22:37:29 +0200

    kan finde konkekroner på felter der ikke er gule

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 
-images = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/2.jpg")
+images = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/41.jpg")
 
 # Crown template
 temp_up = cv2.imread("Croped_crown.jpg")
@@ -15,15 +15,19 @@ temp_right = cv2.imread("Croped_crown_right.jpg")
 #-----------------------------------------------------------------------------------------------
 matched_up = cv2.matchTemplate(images, temp_up, cv2.TM_CCOEFF_NORMED)
 output_up = cv2.normalize(matched_up, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+output_up_uint8 = output_up.astype("uint8")
 
 matched_down = cv2.matchTemplate(images, temp_down, cv2.TM_CCOEFF_NORMED)
 output_down = cv2.normalize(matched_down, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+output_down_uint8 = output_down.astype("uint8")
 
 matched_left = cv2.matchTemplate(images, temp_left, cv2.TM_CCOEFF_NORMED)
 output_left = cv2.normalize(matched_left, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+output_left_uint8 = output_left.astype("uint8")
 
 matched_right = cv2.matchTemplate(images, temp_right, cv2.TM_CCOEFF_NORMED)
 output_right = cv2.normalize(matched_right, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+output_right_uint8 = output_right.astype("uint8")
 #-----------------------------------------------------------------------------------------------
 # Get variables after template matching
 mainValUp, maxValUp, minLocUp, maxLocUp = cv2.minMaxLoc(matched_up)
@@ -33,30 +37,63 @@ minValR, maxValR, minLocR, maxLocR = cv2.minMaxLoc(matched_right)
 #-----------------------------------------------------------------------------------------------
 # Thresholding if template match is high enough
 #-----------------------------------------------------------------------------------------------
-if maxValUp > 0.70: 
-    ret, threshold_up = cv2.threshold(output_up,220,255,cv2.THRESH_BINARY)
-    cv2.imshow("crown up", threshold_up)
+
+#print("up", maxValUp)
+#print("down", mainValDown)
+#print("left", maxValL)
+#print("right", maxValR)
+
+
+
+if maxValUp > 0.75: 
+    ret, threshold_up = cv2.threshold(output_up_uint8,220,255,cv2.THRESH_BINARY)
+    #cv2.imshow("crown up", threshold_up)
+    # bounding boxes
+    contours, _ = cv2.findContours(threshold_up, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # draw boxes
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(images, (x, y), (x+w, y+h), (0, 255, 0), 2)
 else:
     print("no crown pointing up")
 
-if maxValDown > 0.85:    
-    ret, threshold_down = cv2.threshold(output_down,220,255,cv2.THRESH_BINARY)
-    cv2.imshow("crown down", threshold_down)
+if maxValDown > 0.75:    
+    ret, threshold_down = cv2.threshold(output_down_uint8,220,255,cv2.THRESH_BINARY)
+    #cv2.imshow("crown down", threshold_down)
+    # bounding boxes
+    contours, _ = cv2.findContours(threshold_down, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # draw boxes
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(images, (x, y), (x+w, y+h), (0, 255, 0), 2)
 else:
     print("no crown pointing down")
 
-if maxValL > 0.85:
-    ret, threshold_left = cv2.threshold(output_left,220,255,cv2.THRESH_BINARY)
-    cv2.imshow("crown left", threshold_left)
+if maxValL > 0.75:
+    ret, threshold_left = cv2.threshold(output_left_uint8,220,255,cv2.THRESH_BINARY)
+    #cv2.imshow("crown left", threshold_left)
+    # bounding boxes
+    contours, _ = cv2.findContours(threshold_left, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # draw boxes
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(images, (x, y), (x+w, y+h), (0, 255, 0), 2)
 else:
     print("no crown pointing left")
 
-if maxValR > 0.70:    
-    ret, threshold_right = cv2.threshold(output_right,220,255,cv2.THRESH_BINARY)
-    cv2.imshow("crown right", threshold_right)
+if maxValR > 0.75:    
+    ret, threshold_right = cv2.threshold(output_right_uint8,220,255,cv2.THRESH_BINARY)
+    #cv2.imshow("crown right", threshold_right)
+    # bounding boxes
+    contours, _ = cv2.findContours(threshold_right, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # draw boxes
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(images, (x, y), (x+w, y+h), (0, 255, 0), 2)
 else:
     print("no crown pointing right")
 
+cv2.imshow("test", images)
 cv2.waitKey()
 cv2.destroyAllWindows()
 #-----------------------------------------------------------------------------------------------
