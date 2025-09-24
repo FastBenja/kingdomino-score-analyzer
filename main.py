358332a4e498040a1a221ab40591a70f37bd2969
Benjamin Hermansen <bshermansen@gmail.com>
2025-09-24 13:35:00 +0200

    Added a testing framework and correct scores for testing purposes

--- main.py
+++ main.py
@@ -3,13 +3,33 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 
-root = tk.Tk()
-root.withdraw()
+"""Class to load images and evaluate them
 
-paths = filedialog.askopenfilenames()
+"""
+class ImageScore:
+    def __init__(self):
+        root = tk.Tk()
+        root.withdraw()
+        self.paths = filedialog.askopenfilenames()
+        
+        self.image_dict = {} # Init dictonary to hold images and their filenames
+        
+        for path in self.paths: # Import selected images
+            path_segments = path.split('/')
+            pretty_path = path_segments[len(path_segments)-1]
+            self.image_dict[pretty_path] = np.array(cv2.imread(path))
+            #print(f"Imported {pretty_path}")
 
-images = [None] * len(paths)
+    def eval_raw_img(self, img): # Function to evaluate an image
+            score = 60
+            return score
+    
+    def run(self): # Run evaluation on all images
+        res = {}
+        for file, img in self.image_dict.items():
+             res[file] = self.eval_raw_img(img)
+        return res
 
-for num, path in enumerate(paths):
-    print(num)
-    images[num] = cv2.imread(path)
+if __name__ == "__main__":
+    test = ImageScore()
+    test.run()
---
+++ test/pass_fail.py
@@ -0,0 +1,40 @@
+import sys
+import os
+import csv
+import numpy as np
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # Add directory above to path, to allow import of main
+from main import ImageScore # Import main class to extend functionality
+
+
+class PassFailTest(ImageScore): # Test class
+    def __init__(self):
+        super().__init__()
+        self.total_tests = 0
+        self.correct_tests = 0
+        self.answers = {}
+        with open("test/results.csv", newline="\n") as csvfile: # Load answers from csv-file
+            reader = csv.reader(csvfile)
+            for answer in reader:
+                self.answers[answer[0]] = int(answer[1]) # Store answers in dictonary with filename as key and score as value
+
+    def pass_fail_test(self): # Run pass/fail test
+        print(f"\n{'='*40}\nStarting pass/fail test with {len(self.image_dict)} images\n{'='*40}\n")
+        for file, img in self.image_dict.items(): # Iterate over all images
+            calculated_answer = self.eval_raw_img(img) # Get calculated score
+            correct_answer = self.answers[file] # Get correct score from dictonary
+            if calculated_answer == correct_answer:
+                print(f"✅ PASS File: {file} calculated score {calculated_answer} expected score {correct_answer}")
+                self.correct_tests += 1
+            else:
+                print(f"❌ FAIL File: {file} calculated score {calculated_answer} expected score {correct_answer}")
+            self.total_tests += 1
+        
+        correct_pct = np.divide(self.correct_tests, self.total_tests) * 100
+        print(f"\n{"="*40}\nPassed {self.correct_tests} out of {self.total_tests} tests ({correct_pct:.2f}%)\n{"="*40}\n")
+        
+            
+
+if __name__ == "__main__":
+    test = PassFailTest()
+    test.pass_fail_test()
