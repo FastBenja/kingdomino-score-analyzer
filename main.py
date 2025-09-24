bd5459e64581734aa1b1288b2c04ec8fe5377585
Santhosh Udayakumar <93641228+Sanu546@users.noreply.github.com>
2025-09-24 08:43:50 +0200

    Spilt to tilles

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import tkinter as tk
 from tkinter import filedialog
 
+W_size = 5
+H_size = 5
+
 root = tk.Tk()
 root.withdraw()
 
@@ -13,3 +16,33 @@ images = [None] * len(paths)
 for num, path in enumerate(paths):
     print(num)
     images[num] = cv2.imread(path)
+    
+
+tiles = [[None]*H_size]*W_size
+
+images[0] = np.array(images[0])
+
+height, width, channels  = images[0].shape
+
+
+
+for  ih in range(H_size):
+    for iw in range(W_size):
+        x = width/W_size * iw 
+        y = height/H_size * ih
+        h = (height / H_size)
+        w = (width / W_size )
+        print(x,y,h,w)
+        tiles[iw][ih] = images[0][int(y):int(y+h), int(x):int(x+w)]
+        cv2.imshow(f"images[{iw},{ih}]",tiles[iw][ih])
+        
+       
+
+
+
+
+cv2.imshow("dkdk",images[0])
+cv2.waitKey() 
+    
+
+
