95153eff67c3b333cb5deca89b4719a834325f21
Petur Hammer <hammerpetur@gmail.com>
2025-09-25 15:32:12 +0200

    lavet en ekstra fil til at fucke fuldstændigt op

--- Template_test.py
+++ Template_test.py
@@ -1,9 +1,7 @@
 import cv2
 import numpy as np
-import tkinter as tk
-from tkinter import filedialog
 
-images = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/53.jpg")
+images = cv2.imread("King Domino dataset/Cropped and perspective corrected boards/3.jpg")
 
 # Crown template
 temp_up = cv2.imread("Croped_crown.jpg")
@@ -89,30 +87,3 @@ cv2.imshow("test", images)
 cv2.waitKey()
 cv2.destroyAllWindows()
 #-----------------------------------------------------------------------------------------------
-
-
-# Patch-size
-patch_w, patch_h = 100, 100
-
-# list for patches
-roi_zone = [[None for _ in range(5)] for _ in range(5)]
-
-# Loop to crop the image into 25 patches
-for y in range(5):
-    for x in range(5):
-        x_start = x * patch_w
-        y_start = y * patch_h
-        x_end = x_start + patch_w
-        y_end = y_start + patch_h
-
-        cropped_img = images[y_start:y_end, x_start:x_end]
-
-        roi_zone[y][x] = cropped_img
-
-        """
-        #vis hver patch
-        cv2.imshow(f"Patch {y},{x}", cropped_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        """
-
